f53c405ae56f8b2f7dd2b813aa801b90c710e241
Ben Stein-Lobovits <ben@ripple.com>
2016-05-06 13:15:08 -0700

    initial v2 inner pages

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,34 @@
 (function($){
 
-  $(function(){
+  function supportsImports() {
+    return 'import' in document.createElement('link');
+  }
 
-    $('.x').click(function(){
-      $('header').fadeOut('fast');
+  if (supportsImports()) { // HTML5 Import Support
+
+    var link = document.querySelector('link[rel="import"]');
+
+    // Clone the <template> in the import.
+    var template = link.import.querySelector('template');
+    var clone = document.importNode(template.content, true);
+
+    document.querySelector('#sidebarContent').appendChild(clone);
+
+  } else { //jQuery fallback
+
+    $(function(){
+      $("#sidebarContent").load("docs-sidebar.html"); // load docs-sidebar.html in docs sidebar
     });
 
+  }
 
-    setTimeout(function(){
-      $('header').fadeIn('slow');
-      $('header').addClass('animate');
-    }, 500);
+  //Add active class to sidebar links
+  function sidebarActiveClass() {
+    var curentFile = window.location.pathname.split("/").pop();
+    $('.nav-sidebar > li > a[href="' + curentFile + '"]').parent().addClass('active');
+  }
 
-  });
+  sidebarActiveClass();
+
 
 })(jQuery)
