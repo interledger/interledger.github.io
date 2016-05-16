e548cab9da3f330b3e27c23d3e0a1aa3300b88eb
Ben Stein-Lobovits <ben@ripple.com>
2016-05-16 16:27:21 -0700

    scrollspy page example

--- js/custom.js
+++ js/custom.js
@@ -31,14 +31,4 @@
   sidebarActiveClass();
 
 
-  //homepage type effect
-  $(function(){
-    console.log('start');
-      $(".typewriter").typed({
-        strings: ["Blockchain.", "Ledgers."],
-        typeSpeed: 0
-      });
-  });
-
-
 })(jQuery)
