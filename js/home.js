9451e0ae33a250b2ae4bb3a78a786ac648bab9cb
Ben Stein-Lobovits <ben@ripple.com>
2016-05-19 13:24:40 -0700

    fix typo and add why-interledger to sidebar

---
+++ js/home.js
@@ -0,0 +1,43 @@
+(function($){
+
+  //typewriter
+  $(function(){
+    $(".typewriter").typed({
+      stringsElement: $('#typed-strings'),
+      typeSpeed: 50,
+      loop: true,
+    });
+  });
+
+  //on-off switch
+  $(function() {
+    $(".onoffswitch-label").click(function() {
+      $('body').toggleClass('dark');
+    });
+  });
+
+  //syntax highlighting
+  hljs.initHighlightingOnLoad();
+
+  //canvas animation
+  var canvas, stage, exportRoot;
+
+  function init() {
+    canvas = document.getElementById("canvas");
+    exportRoot = new lib.AnimateCC_Test();
+
+    stage = new createjs.Stage(canvas);
+    stage.addChild(exportRoot);
+    stage.update();
+
+    createjs.Ticker.setFPS(lib.properties.fps);
+    createjs.Ticker.addEventListener("tick", stage);
+  }
+
+  //initialize canvas on load
+  $('body').load(function() {
+    init();
+  });
+
+
+})(jQuery)
