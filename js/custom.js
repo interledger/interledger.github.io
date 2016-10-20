9cb58d9e52782c80f55f592fb686a99ff9cbbf8a
adrianhopebailie <adrian@hopebailie.com>
2016-10-20 10:09:24 +0200

    Make sidebar an include file Fix links to InterledgerJS repos

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,5 @@
 (function($){
 
-  function supportsImports() {
-    return 'import' in document.createElement('link');
-  }
-
-  if (supportsImports()) { // HTML5 Import Support
-
-    var link = document.querySelector('link[rel="import"]');
-
-    // Clone the <template> in the import.
-    var template = link.import.querySelector('template');
-    var clone = document.importNode(template.content, true);
-
-    document.querySelector('#sidebarContent').appendChild(clone);
-
-  } else { //jQuery fallback
-
-    $(function(){
-      $("#sidebarContent").load("docs-sidebar.html"); // load docs-sidebar.html in docs sidebar
-    });
-
-  }
-
   //Add active class to sidebar links
   function sidebarActiveClass() {
     var curentFile = window.location.pathname.split("/").pop();
