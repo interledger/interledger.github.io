ce8fb9d34ce4f635e26c60a691d06e3f1eb58d0e
Jake Bonham <jake@docforce.io>
2019-08-21 09:36:04 -0700

    Close mobile menu on scroll

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,12 +4,24 @@
   $('[data-toggle="collapse"]').on('click', function() {
     $navMenuCont = $($(this).data('target'));
     $navMenuCont.toggleClass('show');
-    $(".menu-overlay").toggleClass('active');
+    // $(".menu-overlay").toggleClass('active');
   });
   $(".menu-overlay").click(function(event) {
     $(".navbar-toggler").trigger("click");
   });
 
+  //Close menu if open on scroll
+  $prevScrollpos = window.pageYOffset;
+  $(window).scroll(function() {
+
+    $currentScrollPos = window.pageYOffset;
+    if ($prevScrollpos < $currentScrollPos) {
+      if ( $("#navbarHolder").hasClass("show") )  {
+        $(".navbar-toggler").trigger("click");
+      };
+    }
+    $prevScrollpos = $currentScrollPos;
+  })
 
 
   //Add active class to sidebar links
